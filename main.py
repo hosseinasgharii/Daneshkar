f5b298df80f4a3c28340a5c2aaad8fc42aabc34b
Hw3_hosseinA <ho3ein.m1991@gmail.com>
2023-05-23 20:04:47 +0330

    style: docstring and ...

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def apply_discount(price, discount):
+def apply_discount(price: int, discount: float = 0.0)-> int:
+    """calculate"""
     final_price = int(price *(1- discount))
     if not 0 <= final_price < price:
         raise ValueError("Invalid Discount")
