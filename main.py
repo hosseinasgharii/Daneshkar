4644d55dcdd4b40cd6705ec6528f2640b27d6035
Hw3_hosseinA <ho3ein.m1991@gmail.com>
2023-05-23 20:30:00 +0330

    func write integer  and apply_discount

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+class DiscountError(Exception):
+    pass
+
+def integer_discount(function):
+
+    def wrapper(price: int, discount:int)-> int:
+        return function(price , discount / 60)
+    
+    return wrapper
+
+
+@integer_discount  #میاد رفتارشو به اون دکوریتورمون دگ تغییر میده
 def apply_discount(price: int, discount: float = 0.0)-> int:
-    """calculate"""
+    """calculates the final price after discount"""
     final_price = int(price *(1- discount))
     if not 0 <= final_price < price:
         raise ValueError("Invalid Discount")
     
     return final_price
+
+
